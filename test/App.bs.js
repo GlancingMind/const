eb8edeba8fc5dde038dc075f7be542b582187945
Sascha <GlancingMind@outlook.com>
2021-05-21 16:58:37 +0200

    Fix missing whitespace for test output

--- test/App.bs.js
+++ test/App.bs.js
@@ -4,14 +4,14 @@
 function $$default(zora) {
   zora.test("Should run a test synchronously", (function (test) {
           test.equal(3.14, 3.14, "Should be a tasty dessert");
-
+          
         }));
-
+  
 }
 
 export {
   $$default ,
   $$default as default,
-
+  
 }
 /* No side effect */
